ee337b50b1d38005d122dfa29edccc20117ffaa0
Abraham Alanya <alanyaoficial@gmail.com>
2021-09-03 17:30:54 -0500

    data image

---
+++ data.js
@@ -0,0 +1,45 @@
+/* {
+    name: 'photo1',
+    url: 'https://cutt.ly/XWQny76-aa',
+    description: 'Texto de prueba de una imagen. (descripción aleatoria)'
+},
+{
+    name: 'photo2',
+    url: 'https://cutt.ly/KWQnjH7',
+    description: 'Texto de prueba de una imagen. (descripción aleatoria)'
+},
+{
+    name: 'photo3',
+    url: 'https://cutt.ly/KWQnvGB',
+    description: 'Texto de prueba de una imagen. (descripción aleatoria)'
+},
+{
+    name: 'photo4',
+    url: 'https://cutt.ly/JWQQY2N',
+    description: 'Texto de prueba de una imagen. (descripción aleatoria)'
+},
+{
+    name: 'photo5',
+    url: 'https://cutt.ly/0WQQIC3',
+    description: 'Texto de prueba de una imagen. (descripción aleatoria)'
+},
+{
+    name: 'photo6',
+    url: 'https://cutt.ly/RWQQH6E',
+    description: 'Texto de prueba de una imagen. (descripción aleatoria)'
+},
+{
+    name: 'photo7',
+    url: 'https://cutt.ly/4WQQ38v',
+    description: 'Texto de prueba de una imagen. (descripción aleatoria)'
+},
+{
+    name: 'photo8',
+    url: 'https://cutt.ly/7WQQ478',
+    description: 'Texto de prueba de una imagen. (descripción aleatoria)'
+},
+{
+    name: 'photo9',
+    url: 'https://cutt.ly/FWQQ5NP',
+    description: 'Texto de prueba de una imagen. (descripción aleatoria)'
+} */
