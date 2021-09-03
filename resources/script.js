c896be8278c83f4d8c63bed0c64211453ef032c7
Abraham Alanya <alanyaoficial@gmail.com>
2021-09-03 12:41:50 -0500

    integrating vue2

---
+++ resources/script.js
@@ -0,0 +1,12 @@
+var aap = new Vue({
+    el: '#image_card',
+    data: {
+        images_data: [
+            {
+                name: 'demo1',
+                url: 'https://image.flaticon.com/icons/png/512/4897/4897054.png',
+                description: 'Texto de prueba de una imagen. (descripción aleatoria)'
+            }
+        ]
+    }
+});
